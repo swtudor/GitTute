797437273d33b50a6ab1e6ed38e3b830ace27111
Sylwinn Tudor <swtudor@gmail.com>
2021-04-27 13:24:25 -0500

    phrases

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,7 @@ public class Main {
         String greeting = "It's a beautiful day!";
         String name = "Jane Doe";
         System.out.println("Hello, git! I'm " + name + " and " + greeting );
+        System.out.println("Newer phrases added here");
+        System.out.println("more lovely things to learn");
     }
 }
