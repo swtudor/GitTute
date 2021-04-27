cf8da250a100125c17fdb094fc59f0469e4674ae
Sylwinn Tudor <swtudor@gmail.com>
2021-04-27 13:12:24 -0500

    added a greeting

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args){
+        String greeting = "It's a beautiful day!";
         String name = "Jane Doe";
-        System.out.println("Hello, git! I'm " + name );
+        System.out.println("Hello, git! I'm " + name + " and " + greeting );
     }
 }
